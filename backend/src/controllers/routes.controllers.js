c964cb7bdf2b746d047665cc53e1f404c3923d4e
varshacharappalli <167790510+varshacharappalli@users.noreply.github.com>
2025-03-06 09:27:32 +0530

    Updated route.controllers.js

--- backend/src/controllers/routes.controllers.js
+++ backend/src/controllers/routes.controllers.js
@@ -1,11 +1,19 @@
-export const language=(req,res)=>{
-    
-}
+export const language = (req, res) => {
+    const { lang } = req.body;
+    if (!lang) {
+        return res.status(404).json({ message: 'Language is not entered.' });
+    }
+    res.status(200).json({ message: 'Language received successfully.' });
+};
 
-export const scenario=(req,res)=>{
-
-}
+export const scenario = (req, res) => {
+    const { scenario } = req.body;
+    if (!scenario) {
+        return res.status(404).json({ message: 'Scenario has not been specified.' });
+    }
+    res.status(200).json({ message: 'Scenario received successfully.' });
+};
 
 export const conversation=(req,res)=>{
-
+    
 }
